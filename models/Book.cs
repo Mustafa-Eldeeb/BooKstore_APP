f07b99a36aaa0e4b2b8fece4ae42e82aa47b27b2
Mustafa_Eldeeb <darshm466@gmail.com>
2020-08-25 00:34:47 +0200

    adding Author and book models

---
+++ models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public int Description { get; set; }
+
+
+        public Author Author { get; set; }
+    }
+}
