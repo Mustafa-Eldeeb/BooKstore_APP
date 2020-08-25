0de35f5c73512d891b767b983a398a0b6d5f342b
Mustafa_Eldeeb <darshm466@gmail.com>
2020-08-25 02:30:25 +0200

    Add class BookRepository and implement interface

---
+++ models/repo/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.models.repo
+{
+    public class BookRepository : IBookstoreRepository<Book>
+    {
+        public void add(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Book> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ models/repo/IBookstoreRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.models.repo
+{
+    interface IBookstoreRepository<TEntity>
+    {
+        IList<TEntity> List();
+        TEntity Find(int id);
+        void add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(int id);
+    }
+}
